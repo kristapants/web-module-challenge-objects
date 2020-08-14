736c22c31fe4c4937fa816e7e51b9fcfe9043566
Krista <peachykeen@gmail.com>
2020-08-13 21:07:28 -0500

    finished with stretch tasks

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 ///////////////Menu Items (MVP)///////////////////
 
 const latte = {name: "Cafe Latte", price: 4, category: "Drinks"};
-const burger = {name: "Burger", price: 18, category: "Lunch", discount: function(discountCode){
-  if(discountCode === "teacher" || discountCode === "student"){
+const burger = {
+  name: "Burger", 
+  price: 18, 
+  category: "Lunch", 
+  discount: function (discountCode) {
+    if (discountCode === "teacher" || discountCode === "student") {
       this.price = this.price * 0.75;
-    }else if(discountCode === "public"){
+    } else if (discountCode === "public") {
       this.price = this.price * 0.90; 
-    }else{
+    } else {
       this.price = 'Invalid discount code';
     }
     return this.price
@@ -21,8 +25,12 @@ const breakfastBurrito = {name: "Breakfast Burrito", price: 16, category:"Breakf
 
 /* Task 1a: write a function to return more menu items with the same format as the items above. */
 
-function createMenuItem(_name, _cost, _category){
-  return {name: _name, price: _cost, category: _category}
+function createMenuItem(_name, _cost, _category) {
+  return {
+    name: _name, 
+    price: _cost, 
+    category: _category
+  };
 };
 
 
@@ -54,7 +62,7 @@ and should return a number.
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
 
-console.log(burger.discount("student"));
+// burger.discount("student");
 
 
 
@@ -92,7 +100,11 @@ console.log(reviews[5].feedback)
 
 /* Task 4: Add a new rating with your (fictitious) opinions of the restaurant in the same format as the reviews above. */
 
-reviews.push({name : "Lorna", rating : 5, feedback: "I found the menu hard to navigate as it was peices of paper strewn about the store"});
+reviews.push({
+  name : "Lorna", 
+  rating : 5, 
+  feedback: "I found the menu hard to navigate as it was peices of paper strewn about the store"
+});
 
 // console.log(reviews)
 
@@ -135,7 +147,7 @@ function getReviewByIndex(array, index) {
   return `${review.name} gave the restaurant a ${review.rating}, and their feedback was: ${review.feedback}`;
 }
 
-  console.log(getReviewByIndex(reviews, 5));
+//getReviewByIndex(reviews, 5);
 
 
 
@@ -159,7 +171,7 @@ function getLastReview(array) {
   return `${review.name} gave the restaurant a ${review.rating}, and their feedback was: ${review.feedback}`;
 }
 
- //console.log(getLastReview(reviews));
+ //getLastReview(reviews);
 
 
 
@@ -182,11 +194,27 @@ function getLastReview(array) {
     {name:"Lauren", rating: 4, feedback: "Absolutely love that they have karaoke Fridays! Food and drink selection is okay."}]
 */
 
- function getReviewByRating(/* code here */) {
-    /* code here */
+ function getReviewByRating(array, _rating) {
+   const filteredReviews = [];
+    for(i = 0; i < array.length; i++){
+     if(array[i].rating >= _rating && array[i].rating < _rating + 1){
+        filteredReviews.push(array[i]);
+     } 
+   } 
+   return filteredReviews;
   }
 
-  
+//getReviewByRating(reviews, 4);
+
+
+
+
+
+
+
+
+
+
 /** STRETCH 2: Write a function called 'getLongestReview' that returns an array containing all reviews longer than 15 words. 
   
 Your function should accept: 
@@ -200,10 +228,23 @@ and should return an array of objects.
     {name: "Brett", rating: 3, feedback: "great selection of snacks and a nice cafe area to get work done during the day."},
     {name: "Julius", rating: 2, feedback: "I was largely unimpressed by this venue. Nothing special on the menu and too expensive. The atmosphere is polarizing, and not for me, but I think some would like it." }]
 */
-  function getLongReviews(/* code here */) {
-    /* code here */
+
+function getLongestReview(array) {
+  const verboseReviews = [];
+  for(i = 0; i < array.length; i++){
+    if(array[i].feedback.split(" ").length > 15){
+      verboseReviews.push(array[i]);
+    }
   }
-  
+  return verboseReviews;
+}
+
+ //getLongestReview(reviews);
+
+
+
+
+
 
 /* STRETCH 3:  This challenge is not related to the data above! 
 
@@ -223,7 +264,25 @@ The returned object should have the following characteristics:
 */
 
 
-function carMaker(/* code here */) {
-    /* code here */
-    
+function carMaker(milesDriven) {
+  const car = {
+    odometer: milesDriven,
+    drive: function (distance) {
+      this.odometer += distance;
+      return this.odometer;
+    }
+  };
+
+  return car;
 }
+
+const car = carMaker(50);
+
+console.log(car.odometer);
+
+car.drive(300);
+
+console.log(car.odometer);
+
+
+
